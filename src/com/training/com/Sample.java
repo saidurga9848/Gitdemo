c8bc4522bd595f7dc2c2c0b39011cc9dbc19830f
ADMIN <ADMIN@DESKTOP-VC0C62E>
2022-12-29 10:36:46 +0530

    Base project

---
+++ src/com/training/com/Sample.java
@@ -0,0 +1,10 @@
+package com.training.com;
+
+public class Sample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+      System.out.println("This is Github");
+	}
+
+}
